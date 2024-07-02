37fa44939a9f2fac5689fd3987ee8ad0e2d7a624
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-02 15:54:51 -0300

    create customer usecase

---
+++ src/usecase/customer/create/create.customer.usecase.ts
@@ -0,0 +1,30 @@
+import CustomerFactory from "../../../domain/costumer/factory/customer.factory";
+import CustomerRepositoryInterface from "../../../domain/costumer/repository/customer-repository.interface";
+import Address from "../../../domain/costumer/value-object/address";
+import { InputCreateCustomerDto, OutputCreateCustomerDto } from "./create.costumer.dto";
+
+export default class CreateCustomerUseCase {
+    private customerRepository: CustomerRepositoryInterface;
+
+    constructor(customerRepository: CustomerRepositoryInterface) {
+        this.customerRepository = customerRepository;
+    }
+
+    async execute(input: InputCreateCustomerDto): Promise<OutputCreateCustomerDto> {
+        const address = new Address(input.address.street, input.address.number, input.address.zip, input.address.city);
+        const customer = CustomerFactory.createWithAddress(input.name, address);
+
+        await this.customerRepository.create(customer);
+
+        return {
+            id: customer.id,
+            name: customer.name,
+            address: {
+                street: customer.Address.street,
+                number: customer.Address.number,
+                zip: customer.Address.zip,
+                city: customer.Address.city
+            },
+        };
+    }
+}
