18dc7cf1329c067826bc9dde46ed5247a118b242
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-09 10:08:29 -0300

    product validation

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 
 // export default class Product implements ProductInterface {
 export default class Product extends Entity {
@@ -39,27 +40,6 @@ export default class Product extends Entity {
     
 
     validate() {
-        if (this.id.length === 0) {
-            this.notification.addError({ 
-                context: "product",
-                message: "Id is required", 
-            });
-        }
-
-        if (this._name.length === 0) {
-            this.notification.addError({
-                context: "product",
-                message: "Name is required",
-            });
-        }
-
-        if (this._price <= 0) {
-            this.notification.addError({
-                context: "product",
-                message: "Price must be greater than 0",
-            });
-        }
-
-        // return true;
+        ProductValidatorFactory.create().validate(this);
     }
 }
