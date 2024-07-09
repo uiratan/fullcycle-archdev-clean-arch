4aed23a8376cc32ea6ca6d1c4be6431fb45113b9
Uiratan Cavalcante <uiratan@gmail.com>
2024-07-08 21:20:27 -0300

    product notification

--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,23 +1,31 @@
 import Product from "./product";
 
+
 describe("Product unit tests", () => {
        
     it("should throw error when id is empty", () => {
         expect(() => {
             let product = new Product("", "Product 1", 100);
-        }).toThrowError("Id is required");      
+        }).toThrow("product: Id is required");      
     });
 
     it("should throw error when name is empty", () => {
         expect(() => {
             let product = new Product("1", "", 100);
-        }).toThrowError("Name is required");
+        }).toThrow("product: Name is required");
     });
+
+    it("should throw error when id and name are empty", () => {
+		expect(() => {
+			new Product("", "", 100);
+		}).toThrow("product: Id is required,product: Name is required");
+	});
+
 
     it("should throw error when price is less than zero", () => {
         expect(() => {
             let product = new Product("1", "Product 1", -1);
-        }).toThrowError("Price must be greater than 0");
+        }).toThrow("product: Price must be greater than 0");
     });
 
     it("should change name", () => {
